11b722dcbbb7e1abdd4ed9d41a2e20398b0d1d93
gajdos.csanad <gajdos.csanad@BURG-1E05-12>
2025-01-24 10:57:35 +0100

    Done

--- ShoppingCart/Models/ShoppingCart.cs
+++ ShoppingCart/Models/ShoppingCart.cs
@@ -11,6 +11,26 @@
 
         public int ProductCount => _products.Count;
 
+        public void AddProduct(string v1, double v2)
+        {
+            _products.Add(new Product(v1, v2));
+        }
+
+        public List<Product> GetProducts()
+        {
+            return _products;
+        }
+
+        public double GetTotalPrice()
+        {
+            return _products.Sum(x => x.Price);
+        }
+
+        public void RemoveProduct(string v)
+        {
+            _products.Remove(_products.Where(x => x.Name == v).First());
+        }
+
         //TODO Készítse el a ShoppingCart osztályban azokat a metódusokat, amelyekkel sikeresen lefutnak a tesztesetek!
 
     }
